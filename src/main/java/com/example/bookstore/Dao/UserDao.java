3efb637414563cbb0b0acd0ded66dc709badd713
anh <anhnttps14797@fpt.edu.vn>
2023-08-15 18:19:00 +0700

    AddressDao

--- src/main/java/com/example/bookstore/Dao/UserDao.java
+++ src/main/java/com/example/bookstore/Dao/UserDao.java
@@ -2,6 +2,8 @@ package com.example.bookstore.Dao;
 
 import com.example.bookstore.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserDao extends JpaRepository<Users, Long> {
 }
