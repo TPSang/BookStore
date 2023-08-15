31462cc7e9fdd8d106468e02a84987d2c3e14859
Gukk1256 <huyddps22126@fpt.edu.vn>
2023-08-15 18:38:43 +0700

    demo upload

---
+++ src/main/java/com/example/bookstore/Model/Demo.java
@@ -0,0 +1,13 @@
+package com.example.bookstore.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "demo")
+public class Demo {
+}
